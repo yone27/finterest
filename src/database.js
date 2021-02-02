16e82d6dc084f7140d0755d674e2d6c62fe79fe3
yone27 <yonex27000@gmail.com>
2021-02-02 19:02:26 -0430

    first commit

--- src/database.js
+++ src/database.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://yoneiker:yoneiker@cluster0-t41nx.mongodb.net/finterest', {
+mongoose.connect('localhost', {
+// mongoose.connect('mongodb+srv://yoneiker:yoneiker@cluster0-t41nx.mongodb.net/finterest', {
         useCreateIndex: true,
         useNewUrlParser: true,
         useFindAndModify: false
     })
+    mongoose.connect()
     .then(db => console.log("db is connect"))
     .catch(err => console.error(err))
